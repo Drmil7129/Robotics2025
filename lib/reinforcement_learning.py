d189f0e38e347627a0b78f6b23d83208e9f8c2d6
Damil Imran <damil.imran05@gmail.com>
2025-11-12 20:44:30 +0000

    Added simple Q-table action select

--- lib/reinforcement_learning.py
+++ lib/reinforcement_learning.py
@@ -1,65 +1,33 @@
-"""mainController controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-
-from controller import Robot,Motor
 import math
+import numpy as np
 
-TIME_STEP = 16
-TARGET_POINTS_SIZE = 13
-DISTANCE_TOLERANCE = 1.5
-MAX_SPEED = 7.0
-TURN_COEFFICIENT = 4.0
 REWARD_PER_DISTANCE = 1
-autopilot = True
-robot = Robot()
-actions = []
-motors = []
+MAX_X = 50
+MAX_Y = 50
+NUM_OF_ACTIONS = 6
+NUM_OF_DIRECTIONS = 4
+LEARNING_RATE = 0.5
+DISCOUNT_FACTOR = 0.5
+Q_table = np.zeros(MAX_X,MAX_Y,NUM_OF_DIRECTION,NUM_OF_ACTIONS)
 
-def robot_set_speed(left,right):
-  for i in range(4):
-       motors[i + 0].setVelocity(left)
-       motors[i + 4].setVelocity(right)
-       
 
+
+#calculates immediate reward for reaching a state
 def reward_function(prev_distance, next_distance):
     change_in_distance = math.sqrt((prev_distance[0] - next_distance[0])**2 + (prev_distance[1] - next_distance[1])**2)
     reward = change_in_distance * REWARD_PER_DISTANCE
     return reward
     
-def value_function():
-    pass
-
-def q_value_function():
-    pass
+#find which action to do at a state, this favours exploitation over exploration, due to the large Q-table size
+def q_value_action(state):
+    direction = get_direction(state[2])
+    choice = random.randint(0,10)
+    if (choice > 7):
+        action_choice = random.randint(0,NUM_OF_ACTIONS-1)
+    else:
+        action_choice = np.argmax(Q_table[state[0]][state[1]][direction])
+    return action_choice    
     
-def run_autopilot():
-    speeds = [0.0, 0.0]
-    speeds[0] = MAX_SPEED
-    speeds[1] = MAX_SPEED
-    robot_set_speed(speeds[0], speeds[1])
- 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-
-def main():
-    names = ["left motor 1",  "left motor 2",  "left motor 3",  "left motor 4",
-             "right motor 1", "right motor 2", "right motor 3", "right motor 4"]
-    for name in names:
-        motor = robot.getDevice(name)
-        motor.setPosition(float('inf'))
-        motors.append(motor)
-        
-    while robot.step(timestep) != -1:
-        if (autopilot):
-            run_autopilot()
-
-if __name__ == "__main__":
-    main()
+    
+def get_direction(n):
+    return (round(n/90) % 4)
