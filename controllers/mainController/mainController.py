4b9353afc820a688fbc31857b6db6523a0d847f8
Basel <bxa371@student.bham.ac.uk>
2025-11-16 03:35:47 +0000

    Add MDP state representation and action space for RL framework

--- controllers/mainController/mainController.py
+++ controllers/mainController/mainController.py
@@ -5,6 +5,25 @@
 
 from controller import Robot,Motor
 
+
+class RobotState:
+    def __init__(self, position_x=0.0, position_y=0.0, heading=0.0, has_cargo=True):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.heading = heading
+        self.has_cargo = has_cargo
+        
+    def __repr__(self):
+        return f"RobotState(x={self.position_x}, y={self.position_y}, heading={self.heading}, cargo={self.has_cargo})"
+
+ROBOT_ACTIONS = {
+    'FORWARD': 0,
+    'LEFT': 1,
+    'RIGHT': 2,
+    'STOP': 3
+}
+}
+
 TIME_STEP = 16
 TARGET_POINTS_SIZE = 13
 DISTANCE_TOLERANCE = 1.5
